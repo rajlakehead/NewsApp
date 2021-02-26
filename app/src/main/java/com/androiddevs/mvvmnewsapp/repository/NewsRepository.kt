818a0b156248ac75baec2ed0bfb432552a666c37
xRaj18 <68782547+xRaj18@users.noreply.github.com>
2021-02-26 09:22:18 +0530

    Update NewsRepository.kt

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp.repository
 
 import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.models.Article
 
 class NewsRepository(
     val db: ArticleDatabase
@@ -9,8 +10,14 @@ class NewsRepository(
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 
-    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
-        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+    suspend fun getSportNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getSportNews(countryCode, pageNumber)
+
+    suspend fun getScienceNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getScienceNews(countryCode, pageNumber)
+
+    suspend fun getEntertainmentNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getEntertainmentNews(countryCode, pageNumber)
 
     suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
 
